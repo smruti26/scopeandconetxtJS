1d95e205ff64142b58b81142cabeef165cfe4182
Smruti Ranjan <sranjan4@sapient.com>
2018-05-10 15:51:35 +0530

    add contextjs Example

--- scope-context-js/sc-cn.js
+++ scope-context-js/sc-cn.js
@@ -6,43 +6,43 @@
 
 
 
-   // Details of Scope and context :
-   // Below with Example :
+// Details of Scope and context :
+// Below with Example :
 
-	// Example - #1
-
+// Example - #1
+// scope example
 var model = 'BMW'; // gobal Varialble
 
-function_name = function(){
-		return model;
-	};
-	console.log(function_name()); // return the Model of the name variable holding the value (BMW)
-	document.write(function_name() + '<br>');
+function_name = function () {
+	return model;
+};
+console.log(function_name()); // return the Model of the name variable holding the value (BMW)
+document.write(function_name() + '<br>');
 
 // Example - #2
 
-modelname = function(){
+modelname = function () {
 	var model = 'Audi A7'  //private Variable
 };
-	modelname();
-	console.log(modelname()); //  Result of the example 2 is (undefiend)
-	document.write(modelname() + '<br>');
+modelname();
+console.log(modelname()); //  Result of the example 2 is (undefiend)
+document.write(modelname() + '<br>');
 
 
 // Example - #3
- 
-modelname = function (){
-	var model = 'Audi A8' 
+
+modelname = function () {
+	var model = 'Audi A8'
 	return model;
 
 };
-	console.log(modelname()); //  Result of the example 3 is holding the value (model)
-	document.write( modelname()  + '<br>');
+console.log(modelname()); //  Result of the example 3 is holding the value (model)
+document.write(modelname() + '<br>');
 
 //Example - #4
 
 
-modelnamenew = function(){
+modelnamenew = function () {
 	var modelnameNew = 'Mercedes-Benz C -class';
 	return modelnameNew;
 };
@@ -50,9 +50,29 @@ modelnamenew = function(){
 console.log(modelnamenew()); // modelname is showing what the variable is holdig the value.
 document.write(modelnamenew() + '<br>');
 
+//example #5 
 
+// context of JS example
+//obj - #1
+var mercedesBenz = {
+    Company :'MercedesBenz',
+	saymodel : function(){
+		console.log(this.Company);
+		 alert(this.Company);
+	}
+}
+//obj - #2
+var jaguar = {
+    Company :'jaguar',
+	saymodel : function(){
+			console.log(this.Company);
+			 alert(this.Company);
+	}
+}
+mercedesBenz.saymodel();  // refer to #1
 
+jaguar.saymodel(); // refer to #2
 
-
+mercedesBenz.saymodel.apply(jaguar); // we use apply method to bind the obj also
 
 
